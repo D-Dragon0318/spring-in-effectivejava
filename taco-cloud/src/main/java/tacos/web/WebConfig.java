82044b6382389dc2835fc4c77019b175f8f06bdb
code-enjoyment <hwt6018@163.com>
2024-03-20 10:36:48 +0800

    使用视图控制器

---
+++ taco-cloud/src/main/java/tacos/web/WebConfig.java
@@ -0,0 +1,17 @@
+package tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 视图控制器：不需要填充模型或处理输入
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+}
