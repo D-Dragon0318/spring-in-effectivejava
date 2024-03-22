93ba44bb57a27d055da6aba427293bf0759d6644
code-enjoyment <hwt6018@163.com>
2024-03-22 11:47:28 +0800

    添加注释

--- taco-cloud/src/main/java/tacos/data/UserRepository.java
+++ taco-cloud/src/main/java/tacos/data/UserRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	
+	/**
+	 * Spring Data JPA会在运行时自动生成这个接口的实现(DSL语言)。所以，我们现在就可以编写使用该存储库的自定义用户详情服务了。
+	 * 
+	 * @param username
+	 * @return
+	 */
 	User findByUsername(String username);
 
 }
--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -16,6 +16,8 @@ public class SecurityConfig {
 	}
 	
 	/**
+	 * 过滤链条：任何请求都要走到这里执行过滤操作
+	 * 
 	 * 为了能够访问h2的界面，必须设置2个配置：https://www.jianshu.com/p/925d5aece6dc
 	 * 
 	 * 注意：用无痕浏览器哈，否则登录了一次，只要不关闭浏览器权限就会一直存在
@@ -30,7 +32,9 @@ public class SecurityConfig {
 		return http.authorizeRequests().requestMatchers("/design", "/orders").access("hasRole('USER')")
 				.requestMatchers("/", "/**").access("permitAll()")
 
+				//创建自定义的登录页
 				.and().formLogin().loginPage("/login")
+				
 				//开发环境禁用了csrf，因为会影响h2后台界面的访问
 				.and().csrf().disable().build();
 	}
