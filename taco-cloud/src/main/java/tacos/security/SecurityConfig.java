bba182e0402d95c263b33cce24e3746c4091a8b8
code-enjoyment <hwt6018@163.com>
2024-03-22 14:06:18 +0800

    使用书中的方法来实现UserDetailsService接口

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -3,9 +3,14 @@ package tacos.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import tacos.User;
+import tacos.data.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -37,6 +42,22 @@ public class SecurityConfig {
 				
 				//开发环境禁用了csrf，因为会影响h2后台界面的访问
 				.and().csrf().disable().build();
+	}
+	
+	/**
+	 * 写一个UserDetailsService的实现类，并注入到spring容器中：和删除的UserRepositoryUserDetailsService功能是一样的，springSecurity都会自动调用
+	 * 
+	 * @param userRepo
+	 * @return
+	 */
+	@Bean
+	public UserDetailsService userDetailsService(UserRepository userRepo) {
+	  return username -> {
+	    User user = userRepo.findByUsername(username);
+	    if (user != null) return user;
+
+	    throw new UsernameNotFoundException("User '" + username + "' not found");
+	  };
 	}
 
 }
