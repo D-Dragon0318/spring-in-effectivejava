26c2c0126f6db21536b9de153ab8d4a979b96ee1
code-enjoyment <hwt6018@163.com>
2024-03-28 15:01:45 +0800

    新增user的api接口

--- taco-cloud/src/main/java/tacos/api/UserController.java
+++ taco-cloud/src/main/java/tacos/api/UserController.java
@@ -3,22 +3,32 @@ package tacos.api;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import tacos.User;
 import tacos.data.UserRepository;
+import tacos.security.RegistrationForm;
 
 @RestController
 @RequestMapping(path = "/api/users", produces = "application/json")
-//@CrossOrigin(origins = "http://tacocloud:8080")
+@CrossOrigin(origins = "https://www.toolscat.com/")
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@GetMapping(params = "recent")
 	public Iterable<User> recentUsers() {
@@ -28,6 +38,21 @@ public class UserController {
 	@GetMapping("/{id}")
 	public Optional<User> userById(@PathVariable("id") Long id) {
 		return userRepo.findById(id);
+	}
+
+	@GetMapping("/other/{id}")
+	public ResponseEntity<User> userByIdEntity(@PathVariable("id") Long id) {
+		Optional<User> optUser = userRepo.findById(id);
+		if (optUser.isPresent()) {
+			return new ResponseEntity<>(optUser.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User postTaco(@RequestBody RegistrationForm form) {
+		return userRepo.save(form.toUser(passwordEncoder));
 	}
 
 }
