ebd13c43242dea46a7d8128e13a27a47e2fa1414
代码悦读 <hwt6018@163.com>
2024-04-04 22:08:28 +0800

    监听消息

--- taco-cloud/src/main/java/tacos/messaging/listener/UserMessageListener.java
+++ taco-cloud/src/main/java/tacos/messaging/listener/UserMessageListener.java
@@ -1,6 +1,7 @@
 package tacos.messaging.listener;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,14 @@ import tacos.UserVO;
 @Slf4j
 public class UserMessageListener {
 
-  @RabbitListener(queues = "user")
-  public void receiveUser(UserVO user) {
+	@RabbitListener(queues = "user")
+	public void receiveUser(UserVO user) {
 		log.info("监听消息，Receive user: {}", user);
-  }
+	}
+
+	@KafkaListener(topics = "quickstart-events")
+	public void handle(UserVO user) {
+		log.info("监听消息，Receive user: {}", user);
+	}
 
 }
