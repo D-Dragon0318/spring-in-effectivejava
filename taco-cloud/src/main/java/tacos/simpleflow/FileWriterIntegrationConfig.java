c76ab55c420dae424e94a71a9dfaa7f675db7c72
code-enjoyment <hwt6018@163.com>
2024-04-12 12:19:05 +0800

    使用Spring Integration的DSL配置来定义集成流

--- taco-cloud/src/main/java/tacos/simpleflow/FileWriterIntegrationConfig.java
+++ taco-cloud/src/main/java/tacos/simpleflow/FileWriterIntegrationConfig.java
@@ -4,37 +4,49 @@ import java.io.File;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.annotation.Transformer;
-import org.springframework.integration.core.GenericTransformer;
-import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
 
 @Configuration
 public class FileWriterIntegrationConfig {
-	
+
 	/**
 	 * 转换器
+	 * 
 	 * @return
 	 */
-	@Bean
-	@Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
-	public GenericTransformer<String, String> upperCaseTransformer() {
-		return text -> text.toUpperCase();
-	}
-	
+//	@Bean
+//	@Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
+//	public GenericTransformer<String, String> upperCaseTransformer() {
+//		return text -> text.toUpperCase();
+//	}
+
 	/**
 	 * 文件写入消息处理器。
 	 * @return
 	 */
+//	@Bean
+//	@ServiceActivator(inputChannel = "fileWriterChannel")
+//	public FileWritingMessageHandler fileWriter() {
+//		FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("/tmp/code/files"));
+//		handler.setExpectReply(false);
+//		handler.setFileExistsMode(FileExistsMode.APPEND);
+//		handler.setAppendNewLine(true);
+//		return handler;
+//	}
+	
 	@Bean
-	@ServiceActivator(inputChannel = "fileWriterChannel")
-	public FileWritingMessageHandler fileWriter() {
-		FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("/tmp/code/files"));
-		handler.setExpectReply(false);
-		handler.setFileExistsMode(FileExistsMode.APPEND);
-		handler.setAppendNewLine(true);
-		return handler;
-	}
+	  public IntegrationFlow fileWriterFlow() {
+	    return IntegrationFlow
+	        .from(MessageChannels.direct("textInChannel"))    
+	        .<String, String>transform(t -> t.toUpperCase())    
+	        .handle(Files    
+	             .outboundAdapter(new File("/tmp/code/files"))
+	             .fileExistsMode(FileExistsMode.APPEND)
+	             .appendNewLine(true))
+	         .get();
+	  }
 
 }
