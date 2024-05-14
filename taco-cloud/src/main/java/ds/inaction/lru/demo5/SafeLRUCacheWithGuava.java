0cedae45de9e9b20d42c0b74fbb02fabf854046a
code-enjoyment <hwt6018@163.com>
2024-05-14 14:33:30 +0800

    测试refreshAfterWrite

--- taco-cloud/src/main/java/ds/inaction/lru/demo5/SafeLRUCacheWithGuava.java
+++ taco-cloud/src/main/java/ds/inaction/lru/demo5/SafeLRUCacheWithGuava.java
@@ -1,72 +1,54 @@
 package ds.inaction.lru.demo5;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+public class SafeLRUCacheWithGuava {
 
-public class SafeLRUCacheWithGuava<K, V> {
+	private LoadingCache<Long, String> cache;
 
-    private LoadingCache<K, Optional<V>> cache;
+	public SafeLRUCacheWithGuava(int cacheSize, long expireAfterWrite) {
+		cache = CacheBuilder.newBuilder().maximumSize(cacheSize).expireAfterWrite(expireAfterWrite, TimeUnit.MINUTES)
+				.refreshAfterWrite(5, TimeUnit.SECONDS).build(new CacheLoader<Long, String>() {
+					@Override
+					public String load(Long key) throws Exception {
+						// 查询数据库或其他数据源
+						String value = queryFromDatabase(key);
+						return value;
+					}
+				});
+	}
 
-    private final Lock lock = new ReentrantLock();
+	public String get(Long key) {
+		try {
+			return cache.get(key);
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    public SafeLRUCacheWithGuava(int cacheSize, long expireAfterWrite) {
-        cache = CacheBuilder.newBuilder()
-                .maximumSize(cacheSize)
-//                .expireAfterWrite(expireAfterWrite, TimeUnit.SECONDS)
-                .refreshAfterWrite(3, TimeUnit.SECONDS) // 每次写入后10秒尝试刷新
-                .build(new CacheLoader<K, Optional<V>>() {
-                    @Override
-                    public Optional<V> load(K key) throws Exception {
-                        // 查询数据库或其他数据源
-                        V value = queryFromDatabase(key);
-                        if (value != null) {
-                            return Optional.of(value);
-                        } else {
-                            // 即使数据库查询结果为空，也将空值放入缓存，防止穿透
-                            return Optional.empty();
-                        }
-                    }
-                });
-    }
+	public void put(Long key, String value) {
+		cache.put(key, value);
+	}
 
-    public V get(K key) {
-        try {
-            Optional<V> optionalValue = cache.get(key);
-            return optionalValue.orElse(null);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	// 示例查询方法，实际项目中应替换为真实的数据源查询
+	private String queryFromDatabase(Long key) throws InterruptedException {
+		// 这里是模拟数据库查询，实际开发中请替换为真实数据库查询
+		System.out.println("数据库查询");
+        Thread.sleep(1000);
+		return "Value for " + key + ", loaded at: " + System.currentTimeMillis();
+	}
 
-    public void put(K key, V value) {
-        cache.put(key, Optional.of(value));
-    }
-
-    // 示例查询方法，实际项目中应替换为真实的数据源查询
-    private V queryFromDatabase(K key) throws InterruptedException {
-    	// 这里是模拟数据库查询，实际开发中请替换为真实数据库查询
-        // 如果缓存击穿发生，只有一个线程会执行这里的逻辑
-        System.out.println("数据库查询");
-        return (V) ("Value for " + key + ", loaded at: " + System.currentTimeMillis());
-    }
-
-    public Set<K> getCacheKeys(){
-        return cache.asMap().keySet();
-    }
-    
-    public void refreshCache(K key) {
-    	cache.refresh(key);
+	public Set<Long> getCacheKeys() {
+		return cache.asMap().keySet();
 	}
 
 }
-
---
+++ taco-cloud/src/main/java/ds/inaction/lru/demo5/main.java
@@ -0,0 +1,26 @@
+package ds.inaction.lru.demo5;
+
+import java.util.concurrent.*;
+
+public class main {
+	// 使用示例
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		SafeLRUCacheWithGuava cache = new SafeLRUCacheWithGuava(3, 10);
+
+		System.out.println("cache get");
+        String rs = cache.get(10L);
+        System.out.println("cache rs:"+ rs);
+
+        Thread.sleep(6000);
+
+		System.out.println("cache get");
+        rs = cache.get(10L);
+        System.out.println("cache rs:"+ rs);
+        
+        Thread.sleep(3000);
+
+		System.out.println("cache get");
+        rs = cache.get(10L);
+        System.out.println("cache rs:"+ rs);
+	}
+}
