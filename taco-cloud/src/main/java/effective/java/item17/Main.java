60663ac4fdc2a7d368f3c4cfecf95c76992472d4
code-enjoyment <hwt6018@163.com>
2024-05-29 13:34:26 +0800

    注释

--- taco-cloud/src/main/java/effective/java/item17/Main.java
+++ taco-cloud/src/main/java/effective/java/item17/Main.java
@@ -1,8 +1,7 @@
 package effective.java.item17;
 
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		// 创建PhoneNumber实例
 		PhoneNumber phoneNumber = new PhoneNumber("12345678");
 
@@ -25,8 +24,5 @@ public class Main {
 		
 		// 再次打印ContactInfo信息以验证状态是否改变
         System.out.println("ContactInfo Information: " + contact); 
-
-		// 注意：由于PhoneNumber被设计为不可变，此处的“拷贝”更多是出于演示目的。
-		// 在实际不可变设计中，直接返回引用也是安全的，因为原始数据不会被修改。
 	}
 }
